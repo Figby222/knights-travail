e586d1da6e44babce36ff2aa286fcb875a32e4f6
Figby222 <figby222@outlook.com>
2024-03-26 14:52:31 -0700

    Fix GameBoard.build()
    Init array on each index of boardArr
    Push 8 squares to each index of boardArr

--- src/modules/game-board.js
+++ src/modules/game-board.js
@@ -1,14 +1,15 @@
 import Square from './square.js';
 const GameBoard = function() {
-    this.board = build();
+    this.board = this.build();
 }
 
 GameBoard.prototype.build = function() {
-    let boardArr;
+    let boardArr = [];
 
     for (let x = 0; x < 8; x++) {
+        boardArr[x] = [];
         for (let y = 0; y < 8; y++) {
-            boardArr[x][y] = new Square(x, y);
+            boardArr[x].push(new Square(x, y));
         }
     }
     return boardArr;
